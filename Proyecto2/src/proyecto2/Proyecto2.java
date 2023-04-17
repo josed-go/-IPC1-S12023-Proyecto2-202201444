ceff2254910439a246d510ae37cff118405a602a
José Góngora <jdavidgongorao@gmail.com>
2023-04-16 19:07:00 -0600

    Se hizo la vista inicial

---
+++ Proyecto2/src/proyecto2/Proyecto2.java
@@ -0,0 +1,17 @@
+package proyecto2;
+
+import proyecto2.vista.Inicio;
+
+/**
+ *
+ * @author JD
+ */
+public class Proyecto2 {
+
+
+    public static void main(String[] args) {
+        Inicio inicio = new Inicio();
+        inicio.setVisible(true);
+    }
+    
+}
